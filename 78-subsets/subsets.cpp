08f9f3190cda110fc7dae511c10d0c7ec0a0ed0a
jashandhillon90 <24bai70540@cuchd.in>
2026-07-27 10:31:43 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (60.47%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -3,21 +3,21 @@ public:
 
         // tc and sc is n*2pown
 
-       void subsets(vector<vector<int>>& ans,int index,vector<int>&arr,vector<int>& nums){
+       void subsets(vector<vector<int>>& ans,int index,vector<int>&subset,vector<int>& nums){
         if(index==nums.size()){
-            ans.push_back(arr);
+            ans.push_back(subset);
             return;
         }
-        arr.push_back(nums[index]);
-        subsets(ans,index+1,arr,nums);// for inclusion
-        arr.pop_back();
-        subsets(ans,index+1,arr,nums);   //for exclusing
+        subset.push_back(nums[index]);
+        subsets(ans,index+1,subset,nums);// for inclusion
+        subset.pop_back();
+        subsets(ans,index+1,subset,nums);   //for exclusing
        } 
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> arr;
+        vector<int> subset;
         vector<vector<int>> ans;
-        subsets(ans, 0, arr, nums);
+        subsets(ans, 0, subset, nums);
         return ans;
     }
 
