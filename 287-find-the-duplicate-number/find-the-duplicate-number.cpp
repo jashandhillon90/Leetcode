4039550fcbe252afe1868d9f30ca8658cd57f971
jashandhillon90 <24bai70540@cuchd.in>
2026-08-09 00:25:01 +0530

    Time: 67 ms (17.74%) | Memory: 87.9 MB (12.45%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& arr) {
-        int slow=arr[0],fast=arr[0]; //linkedlist assume kr lenge
-        do{
-           slow=arr[slow];//+1
-            fast=arr[arr[fast]];//+2
-        }while(slow!=fast);
-        slow=arr[0]; //dubara first pointer to point krega or next element cycle create krega jab fast slow k equal hoga in linkedlist.
-        while(slow!=fast){
-            slow=arr[slow];//+1
-            fast=arr[fast];//+1
+      int findDuplicate(vector<int>& arr) {
+        unordered_set<int>s;
+        vector<int>ans;
+        for(int i=0;i<arr.size();i++){
+            if(s.find(arr[i])!=s.end()){
+                return arr[i];
+            }
+            s.insert(arr[i]);
+        }
+        return -1;
+     }
+//     int findDuplicate(vector<int>& arr) {
+//         int slow=arr[0],fast=arr[0]; //linkedlist assume kr lenge
+//         do{
+//            slow=arr[slow];//+1
+//             fast=arr[arr[fast]];//+2
+//         }while(slow!=fast);
+//         slow=arr[0]; //dubara first pointer to point krega or next element cycle create krega jab fast slow k equal hoga in linkedlist.
+//         while(slow!=fast){
+//             slow=arr[slow];//+1
+//             fast=arr[fast];//+1
 
-        }return fast;
-    }
-};
+//         }return fast;
+//     }
+ };
