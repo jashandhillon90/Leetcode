bd98204b1ae88567f1567a6b9a0060824ae672bb
jashandhillon90 <24bai70540@cuchd.in>
2026-08-09 00:14:04 +0530

    Time: 7 ms (50.51%) | Memory: 65.1 MB (70.27%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& arr) {
+        int slow=arr[0],fast=arr[0]; //linkedlist assume kr lenge
+        do{
+           slow=arr[slow];//+1
+            fast=arr[arr[fast]];//+2
+        }while(slow!=fast);
+        slow=arr[0]; //dubara first pointer to point krega or next element cycle create krega jab fast slow k equal hoga in linkedlist.
+        while(slow!=fast){
+            slow=arr[slow];//+1
+            fast=arr[fast];//+1
+
+        }return fast;
+    }
+};
