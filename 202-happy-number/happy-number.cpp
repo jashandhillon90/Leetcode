55b13dc6564c6ad6cc6418c73a5f45bbc46ce7bf
jashandhillon90 <24bai70540@cuchd.in>
2026-08-17 10:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (82.78%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
+   int squaredsum(int n){
+    int sum=0;
+    while(n>0){
+        int r=n%10;
+        sum+=r*r;
+        n/=10;
+    }
+    return sum;
+   }
     bool isHappy(int n) {
-        unordered_set<int> s;
-        while (n != 1) {
-            if (s.find(n) != s.end())
-                return false;
-            s.insert(n);
-            int sum = 0;
-            while (n > 0) {
-                int r = n % 10;
-                sum += r * r;
-                n = n / 10;
-            }
-            n = sum;
+        // unordered_set<int> s;
+        // while (n != 1) {
+        //     if (s.find(n) != s.end())
+        //         return false;
+        //     s.insert(n);
+        //     int sum = 0;
+        //     while (n > 0) {
+        //         int r = n % 10;
+        //         sum += r * r;
+        //         n = n / 10;
+        //     }
+        //     n = sum;
+        // }
+        // return true;
+        // cycle ban rhi then we use fast and slow pointer.
+        int slow=squaredsum(n);
+        int fast=squaredsum(squaredsum(n));
+        while(fast!=slow){
+            slow=squaredsum(slow);
+            fast=squaredsum(squaredsum(fast));
         }
-        return true;
+        return slow==1;
     }
 };
