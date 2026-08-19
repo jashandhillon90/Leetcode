4f4906fe05ea873a81fc85605df1e8d9401f5421
jashandhillon90 <24bai70540@cuchd.in>
2026-08-19 21:53:02 +0530

    Time: 2 ms (32.22%) | Memory: 14.1 MB (34.49%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -2,17 +2,23 @@ class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
         int n=arr.size();
-        vector<int> ans;
-        for(int i=0;i<arr.size();i++){
-            ans.push_back(arr[i]);
+        // vector<int> ans;
+        // for(int i=0;i<arr.size();i++){
+        //     ans.push_back(arr[i]);
+        //     if(arr[i]==0){
+        //         ans.push_back(0);
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     arr[i]=ans[i];
+        // }
+     
+        for(int i=0;i<n;i++){
             if(arr[i]==0){
-                ans.push_back(0);
+                arr.insert(arr.begin()+i,0);
+                arr.pop_back();
+                i++;
             }
         }
-        for(int i=0;i<n;i++){
-            arr[i]=ans[i];
-        }
-     
-        
     }
 };
