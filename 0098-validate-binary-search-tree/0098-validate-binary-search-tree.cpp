aa95af45fdcb1d5d96bdb221855e170839132702
jashandhillon90 <24bai70540@cuchd.in>
2026-09-06 21:58:57 +0530

    Time: 0 ms (100%), Space: 22 MB (44.62%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -23,15 +23,5 @@ public:
   return check(root,NULL,NULL);
       
     }
-// TreeNode* prev = NULL;
-//     bool isValidBST(TreeNode* root) {
-//         if (root == NULL)
-//             return true;
-//         if (!isValidBST(root->left))
-//             return false;
-//         if (prev != NULL && root->val <= prev->val)
-//             return false;
-//         prev = root;
-//         return isValidBST(root->right);
-//     }
+
 };
